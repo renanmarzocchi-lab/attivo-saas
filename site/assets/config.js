3da50d8f9df590fbdaa130cf5e4f49b189f7dc96
Renan Marzocchi <renan@attivocorretora.com.br>
2026-04-06 16:30:46 -0300

    feat: adiciona site institucional isolado em /site sem impactar SaaS

---
+++ site/assets/config.js
@@ -0,0 +1,17 @@
+window.ATTIVO_CONFIG = {
+  googleScriptUrl: 'COLE_AQUI_A_URL_DO_WEBAPP_DO_GOOGLE_APPS_SCRIPT',
+  appsScriptAdminUrl: 'COLE_AQUI_A_URL_DO_PAINEL_MASTER_DO_GOOGLE_APPS_SCRIPT',
+  appsScriptAffiliateUrl: 'COLE_AQUI_A_URL_DO_PAINEL_DO_AFILIADO_DO_GOOGLE_APPS_SCRIPT',
+  siteUrl: 'https://www.attivocorretora.com.br',
+  documentVersion: 'SIC-ATTIVO-2026.03-REV4',
+  documentHash: '822d8a41f4f03aa0c0e91c3187d91cba40cfe74b85ee26cd7de219a9547971cc',
+  documentReleaseDate: '2026-03-24',
+  documentUrl: '/documentos/afiliados/programa-afiliados-attivo.pdf',
+  documentDocxUrl: '/documentos/afiliados/programa-afiliados-attivo.docx',
+  documentTitle: 'Programa de Afiliados ATTIVO',
+  requireDocAcceptance: 'SIM',
+  enforceCurrentDocForRelease: 'SIM',
+  requireDownloadBeforeRelease: 'SIM',
+  embedAppsScriptPanels: 'SIM',
+  autoCommissionOnClosedLead: 'SIM'
+};
